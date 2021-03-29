d127f818a4f3e02fc8bdb3f1d9386eb4e89171b5
Ndumiso_suspect <73614263+Bhekinkosi12@users.noreply.github.com>
2021-03-29 14:44:23 -0700

    Update Address.cs

--- Core/Entities/Address.cs
+++ Core/Entities/Address.cs
@@ -7,7 +7,9 @@ namespace Core.Entities
     public class Address : EntityBase
     {
         public string Street { get; set; }
+        
         public string City { get; set; }
+        
         public int Number { get; set; }
     }
 }
